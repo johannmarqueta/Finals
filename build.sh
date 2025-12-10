437893f4019cbc1faa3354c3b2aeef4a5adca6c6
Johannmarqueta-os <mjamhekisa@gmail.com>
2025-12-10 13:00:37 +0800

    Fix migrations: run in startCommand with bash

--- build.sh
+++ build.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 set -o errexit
 
+echo "Installing dependencies..."
 pip install -r requirements.txt
 
-python manage.py collectstatic --noinput
+echo "Running migrations..."
 python manage.py migrate --noinput
+
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
+
+echo "Build complete!"
